99312324dbda3c666b989cfbac6d5e5a8f4319cd
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-04-27 17:37:56 -0300

    Atualizando component do toastify e adicionando um customId

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 
 import MyRoute from './myRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 export default function Routes() {
-  toast.success('Sucesso mermao');
-  toast.error('Sucesso mermao');
+  // toast.success('Sucesso mermao', {
+  //   toastId: 'successId',
+  // });
+  // toast.error('Sucesso mermao', {
+  //   toastId: 'errorId',
+  // });
   return (
     <Switch>
       {/* component,  Irá rendezirar o componente de Login, que esta em pages
