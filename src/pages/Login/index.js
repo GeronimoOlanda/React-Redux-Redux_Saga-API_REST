403fb2b00ec8d0e570938663ff2ad41d7b075ba2
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-04-29 16:05:24 -0300

    Adicionando hooks para requisição de dados da API

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
-
+import axios from '../../services/axios';
 export default function Login() {
+  /*esse hooks */
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      const { data } = response;
+      console.log(data);
+    }
+    getData();
+  }, []);
   return (
     <Container>
       <Title isRed={true}>
